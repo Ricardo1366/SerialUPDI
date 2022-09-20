b511bd1c0346add83aec38e4dd22a154b5db5130
Ricardo Gil <rgv1366@hotmail.com>
2022-09-20 23:59:37 +0200

    Configurado como receptor "sin sueño"

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <LoRa.h>
+#include <avr/sleep.h>
 
 // Pines módulo LoRa RFM95W
 #define LORA_SSPIN PIN_PA7
@@ -12,7 +13,7 @@
 #define LORA_RESETPIN PIN_PD6
 
 // Pines temporizador TPL5010
-#define TEMP_WAKEPIN  PIN_PC1
+#define TEMP_WAKEPIN PIN_PC1
 #define TEMP_DONEPIN PIN_PC0
 
 #define PINLED DD0
@@ -21,13 +22,17 @@ const long frecuencia = 868E6;    // 868 MHz
 long intervalo = 1000;
 long tiempo = 0;
 volatile bool despierta = false;
+long contadorPaqutes;
 
 void despertar();
 
 void setup() {
+  delay(10000);
   // Configuramos el monitor serial a 115200
   Serial2.begin(115200);
   while (!Serial2);
+  // set_sleep_mode(SLEEP_MODE_STANDBY);
+  // sleep_enable();
 
   // Configuramos el módulo LoRa a 868MHz
   Serial2.println("Inicio configuración LoRa Receiver ");
@@ -37,7 +42,7 @@ void setup() {
     Serial2.println("Inicialización de LoRa ha fallado.");
     while (1);
   }
-
+  
   // Configuramos los pines del temporizador.
   // pinMode(TEMP_WAKEPIN, INPUT);
   pinMode(TEMP_DONEPIN, OUTPUT);
@@ -46,7 +51,7 @@ void setup() {
   pinMode(PINLED, OUTPUT);
 
   // Configuramos la interrupción de control del temporizador
-  attachInterrupt(TEMP_WAKEPIN, despertar, FALLING);
+  attachInterrupt(TEMP_WAKEPIN, despertar, RISING);
 
   // Enviamos señal DONE (Hecho) para evitar el reseteo.
 
@@ -61,21 +66,30 @@ void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     // received a packet
-    Serial2.print("Paquete recibido: '");
+    Serial2.print("Paquete recibido "); Serial2.print(contadorPaqutes++); Serial2.print(" - ") ;Serial2.print(packetSize); Serial2.println(F(" bytes de longitud."));
 
     // read packet
     while (LoRa.available()) {
       Serial2.print((char)LoRa.read());
     }
 
+    if (contadorPaqutes == 5)
+    {
+      contadorPaqutes = 0;
+      delay(200);
+      // sleep_mode();
+      // sleep_disable();
+      Serial2.println(F("¡Despierto!"));
+    }
+    
     // print RSSI of packet
-    Serial2.print("' with RSSI ");
+    Serial2.println();
+    Serial2.print("  Potencia señal = ");
     Serial2.println(LoRa.packetRssi());
     digitalWrite(PINLED, !digitalRead(PINLED));
   }
   if(millis() - tiempo >= intervalo){
     tiempo += intervalo;
-    // Serial2.print("Paso "); Serial2.println(tiempo / intervalo);
   }
   if(despierta){
     digitalWrite(TEMP_DONEPIN, HIGH);
@@ -87,5 +101,6 @@ void loop() {
 
 
 void despertar(){
+  
   despierta = true;
 }
